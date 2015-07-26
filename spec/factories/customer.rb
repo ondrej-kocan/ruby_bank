a299d01931863e4bd03973ff18915dfbe1663d11
Ondrej Kocan <mail@ondrejkocan.sk>
2015-07-26 08:46:48 +0100

    Create customer model

---
+++ spec/factories/customer.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :customer do |f|
+    f.username 'john_doe'
+    end
+end
---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+require 'rails_helper'
+
+RSpec.describe Customer, type: :model do
+
+  it 'has a valid factory' do
+    FactoryGirl.create(:customer).should be_valid
+  end
+
+  it 'is invalid without a username' do
+    FactoryGirl.build(:customer, username: nil).should_not be_valid
+  end
+
+  it 'is invalid with empty username' do
+    FactoryGirl.build(:customer, username: '').should_not be_valid
+  end
+
+  it 'is invalid with short username' do
+    FactoryGirl.build(:customer, username: '0' * 3).should_not be_valid
+  end
+
+  it 'is invalid with long username' do
+    FactoryGirl.build(:customer, username: '0' * 26).should_not be_valid
+  end
+
+  it 'needs to have a unique username'
+
+end
