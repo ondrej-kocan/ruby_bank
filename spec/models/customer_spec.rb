1c17eb39da58db1861acedcff35f224f6fb05a31
Ondrej Kocan <mail@ondrejkocan.sk>
2015-07-26 09:06:38 +0100

    Change deprecated 'should' calls for new expect calls

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,25 +4,25 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
 
   it 'has a valid factory' do
-    FactoryGirl.create(:customer).should be_valid
+    expect(FactoryGirl.create(:customer)).to be_valid
   end
 
   it 'is invalid without a username' do
-    FactoryGirl.build(:customer, username: nil).should_not be_valid
+    expect(FactoryGirl.build(:customer, username: nil)).not_to be_valid
   end
 
   it 'is invalid with empty username' do
-    FactoryGirl.build(:customer, username: '').should_not be_valid
+    expect(FactoryGirl.build(:customer, username: '')).not_to be_valid
   end
 
   it 'is invalid with short username' do
-    FactoryGirl.build(:customer, username: '0' * 3).should_not be_valid
-    FactoryGirl.build(:customer, username: '0' * 4).should be_valid
+    expect(FactoryGirl.build(:customer, username: '0' * 3)).not_to be_valid
+    expect(FactoryGirl.build(:customer, username: '0' * 4)).to be_valid
   end
 
   it 'is invalid with long username' do
-    FactoryGirl.build(:customer, username: '0' * 26).should_not be_valid
-    FactoryGirl.build(:customer, username: '1' * 25).should be_valid
+    expect(FactoryGirl.build(:customer, username: '0' * 26)).not_to be_valid
+    expect(FactoryGirl.build(:customer, username: '1' * 25)).to be_valid
   end
 
   it 'needs to have a unique username' do
@@ -31,16 +31,20 @@ RSpec.describe Customer, type: :model do
 
     second = FactoryGirl.build(:customer, username: first.username)
 
-    first.should be_valid
-    second.should_not be_valid
+    expect(first).to be_valid
+    expect(second).not_to be_valid
   end
 
   it 'is invalid without a password' do
-    FactoryGirl.build(:customer, username: nil, password_digest: nil).should_not be_valid
+    customer = FactoryGirl.create(:customer)
+    customer.password_digest = nil
+    expect(customer).not_to be_valid
   end
 
   it 'is invalid with empty password' do
-    FactoryGirl.build(:customer, username: nil, password_digest: '').should_not be_valid
+    customer = FactoryGirl.create(:customer)
+    customer.password_digest = ''
+    expect(customer).not_to be_valid
   end
 
 end
