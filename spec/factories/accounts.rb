58c41d9f5fe52f3ea996dfbfd2cbeb771e42db1c
Ondrej Kocan <mail@ondrejkocan.sk>
2015-07-26 10:53:50 +0100

    Add failing test for balance attribute on account

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
-  factory :account do
-    
+  factory :account do |a|
+    a.balance 100.0
   end
 
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'is invalid with a negative balance' do
+    a = FactoryGirl.create(:account)
+    a.balance *= -1
+    expect(a).not_to be_valid
+  end
+
 end
