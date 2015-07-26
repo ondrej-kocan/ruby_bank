f3d5397f3e12e7888c1620565842ba3e48037473
Ondrej Kocan <mail@ondrejkocan.sk>
2015-07-26 11:19:02 +0100

    Create a form to make transfers

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -8,4 +8,8 @@ class AccountsController < ApplicationController
     @account = @current_customer.account
   end
 
+  def transfer
+    redirect_to account_url
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root 'sessions#new'
 
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
+  get    'login'     => 'sessions#new'
+  post   'login'     => 'sessions#create'
+  delete 'logout'    => 'sessions#destroy'
 
   get    'account'   => 'accounts#show'
+
+  post   'transfer'  => 'accounts#transfer'
 
 end
